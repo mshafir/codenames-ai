59b956b19d85bd20b11403fce3a59487897fc7be
Michael Shafir <mshafir1@carecorenational.com>
2017-11-15 21:59:00 -0500

    make site mobile friendly

--- deploy_frontend.sh
+++ deploy_frontend.sh
@@ -1,4 +1,4 @@
 cd frontend
 ember build --environment=production
-scp -r dist $1:~/codenames/web
+scp -r dist $1:~/web
 # gunicorn server:app -b 127.0.0.1:5000 -w 4
--- frontend/config/environment.js
+++ frontend/config/environment.js
@@ -47,6 +47,7 @@ module.exports = function(environment) {
 
   if (environment === 'production') {
     // here you can enable a production-specific feature
+    ENV.rootURL = process.env['BASE_URL'] || '/codenames/';
   }
 
   return ENV;
